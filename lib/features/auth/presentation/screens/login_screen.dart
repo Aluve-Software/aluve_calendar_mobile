87a51ab789b2368ef74fd28a8f5820bbaa76cc3c
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-09-18 16:14:45 +0200

    🔧 fix(login_screen.dart): fix import statements and formatting issues ✨ feat(login_screen.dart): improve UI layout and styling for the login screen

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,8 +1,6 @@
-import 'package:aluve_calendar_mobile/features/auth/presentation/widgets/button.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../provider/auth_provider.dart';
+import '../widgets/button.dart';
 import '../widgets/input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -19,172 +17,152 @@ class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height,
-            maxWidth: MediaQuery.of(context).size.width,
-          ),
-          decoration: const BoxDecoration(
-            color: Color(0xffd6b9ed),
-          ),
-          child: Column(
-            children: [
-              const Expanded(
-                flex: 2,
+        body: SafeArea(
+            child: SingleChildScrollView(
                 child: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 5,
+          width: double.infinity,
+          color: const Color(0XFFD6B9ED),
+          child: Image.asset("assets/Aluve_monochrome_black.png"),
+        ),
+        Container(
+            padding: const EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome",
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontSize: 40,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xff202020),
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        "Back.",
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontSize: 40,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xff202020),
+                          // height: 66 / 40,
+                        ),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Email",
+                    style: TextStyle(
+                      fontFamily: "Inter",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff313037),
+                      height: 22 / 14,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                TextInputField(
+                  labelText: "Enter email",
+                  controller: _emailController,
+                  isPassword: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Password",
+                    style: TextStyle(
+                      fontFamily: "Inter",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff313037),
+                      height: 22 / 14,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                TextInputField(
+                  labelText: "Enter password",
+                  controller: _passwordController,
+                  isPassword: true,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Forgot password?",
+                      style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 15.5,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff369ff7),
+                        height: 26 / 15.5,
+                      ),
+                      textAlign: TextAlign.right,
+                    )),
+                const SizedBox(
+                  height: 40,
+                ),
+                Button(text: "Sign In", onPressed: () {}),
+                const SizedBox(
+                  height: 27,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(""),
+                    Text(
+                      "Don’t have an account? ",
+                      style: TextStyle(
+                        fontSize: 16,
+                        height: 22 / 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      " Register.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        height: 22 / 16,
+                        color: Color(0XFF369FF7),
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
                   ],
                 ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    ),
-                  ),
-                  child: Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(33, 50, 33, 30),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Welcome",
-                              style: TextStyle(
-                                fontSize: 40,
-                                fontFamily: "Inter",
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Back.",
-                              style: TextStyle(
-                                fontSize: 40,
-                                fontFamily: "Inter",
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 40),
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Email",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontFamily: "Inter",
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 14,
-                          ),
-                          TextInputField(
-                            controller: _emailController,
-                            isPassword: false,
-                            labelText: 'Enter email',
-                          ),
-                          const SizedBox(
-                            height: 28,
-                          ),
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Password",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontFamily: "Inter",
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 14,
-                          ),
-                          TextInputField(
-                            controller: _passwordController,
-                            isPassword: true,
-                            labelText: 'Enter password',
-                          ),
-                          const SizedBox(
-                            height: 12,
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: GestureDetector(
-                              onTap: () {
-                                // Handle "Forgot Password" action
-                              },
-                              child: const Text(
-                                'Forgot password?',
-                                style: TextStyle(
-                                  color: Color(0xFF369FF7),
-                                  fontSize: 15.5,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: "Inter",
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          Button(
-                            text: "Sign In",
-                            onPressed: () {},
-                          ),
-                          const SizedBox(
-                            height: 27,
-                          ),
-                          const Align(
-                            alignment: Alignment.centerRight,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "Don't have an account? ",
-                                  style: TextStyle(
-                                    fontFamily: "Nunito",
-                                    fontSize: 16,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                Text(
-                                  'Register',
-                                  style: TextStyle(
-                                    fontFamily: "Nunito",
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+              ],
+            ))
+      ],
+    ))));
   }
 }
