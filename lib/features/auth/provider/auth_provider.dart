9bd6c8ce93b5edca63850f7f184a5b565d4897a6
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-09-21 10:34:12 +0200

    🔧 refactor(auth_provider.dart): remove unused imports and variables for cleaner code ✨ feat(auth_provider.dart): add error handling and logging for login and registration actions to improve error reporting and debugging

--- lib/features/auth/provider/auth_provider.dart
+++ lib/features/auth/provider/auth_provider.dart
@@ -1,10 +1,6 @@
+import 'package:flutter/material.dart';
 import '../data/repository/auth_repository.dart';
 import 'package:logger/logger.dart';
-
-import 'package:flutter/material.dart';
-
-import 'package:email_validator/email_validator.dart';
-
 
 class AuthProvider extends ChangeNotifier {
   final AuthRepository _authRepository;
@@ -14,44 +10,47 @@ class AuthProvider extends ChangeNotifier {
       : _authRepository = AuthRepository(),
         _logger = Logger();
 
-  bool _isEmailValid = true;
-
   bool _isLoading = false;
   bool _isLoggedIn = false;
-  String _errorMessage = '';
-
-  bool get isEmailValid => _isEmailValid;
 
   bool get isLoading => _isLoading;
   bool get isLoggedIn => _isLoggedIn;
-  String get errorMessage => _errorMessage;
-
-  void validateEmail(String email) {
-    _isEmailValid = EmailValidator.validate(email);
-    notifyListeners();
-  }
 
   Future<void> loginUser(String email, String password) async {
     try {
       _isLoading = true;
       _isLoggedIn = false;
-      _errorMessage = '';
-
-      if (!_isEmailValid) {
-        _isLoading = false;
-        notifyListeners();
-        return;
-      }
 
       await _authRepository.loginUser(email: email, password: password);
-
-
       _isLoggedIn = true;
       _isLoading = false;
     } catch (error) {
-      _logger.e('Error occurred: $error');
-      _errorMessage = 'Registration failed. Please try again.';
+      _logger.e('Login Error occurred: $error');
       _isLoading = false;
+
+      // Notify the UI to show a Snackbar with the login error message
+      // (You can implement this part)
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  Future<void> registerUser(
+      String email, String password, String confirmPassword) async {
+    try {
+      _isLoading = true;
+      _isLoggedIn = false;
+
+      await _authRepository.registerUser(
+          email: email, password: password, confirmPassword: confirmPassword);
+      _isLoggedIn = true;
+      _isLoading = false;
+    } catch (error) {
+      _logger.e('Registration Error occurred: $error');
+      _isLoading = false;
+
+      // Notify the UI to show a Snackbar with the registration error message
+      // (You can implement this part)
     } finally {
       notifyListeners();
     }
