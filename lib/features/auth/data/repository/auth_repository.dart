96eaf0f9df7c7740a183327b8f82b3367b12f239
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-09-21 10:34:54 +0200

    🔧 chore(auth_repository.dart): add registerUser method to AuthRepository for user registration functionality

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -9,4 +9,11 @@ class AuthRepository {
       {required String email, required String password}) async {
     await authService.loginUser(email, password);
   }
+
+  Future<void> registerUser(
+      {required String email,
+      required String password,
+      required String confirmPassword}) async {
+    await authService.registerUser(email, password, confirmPassword);
+  }
 }
