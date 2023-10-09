18480c78243ea5fa716c0d3a480867b625e65fc7
Sibusiso <sbudahnkoane1@gmail.com>
2023-10-09 18:15:49 +0200

    Add app reusable colors

---
+++ lib/Views/Widgets/app_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color black = const Color(0xFF202020);
+Color lightPurple = const Color(0xFFD6B9ED);
+Color white = const Color(0xFFFFFFFF);
+Color blue1 = const Color(0xFF1877F2);
+Color blue2 = const Color(0xFF369FF7);
+Color purpleGrey = const Color(0xFF776782);
+Color red = const Color(0xFFE33629);
+Color green = const Color(0xFF319F43);
