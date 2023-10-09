630dd496e637acb25fc9a06b7b02df8a8bdecec3
Sibusiso <sbudahnkoane1@gmail.com>
2023-10-09 18:52:04 +0200

    Add register options screen boilerplate

---
+++ lib/Views/register_options_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterOptionsScreen extends StatefulWidget {
+  const RegisterOptionsScreen({super.key});
+
+  @override
+  State<RegisterOptionsScreen> createState() => _RegisterOptionsScreenState();
+}
+
+class _RegisterOptionsScreenState extends State<RegisterOptionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
