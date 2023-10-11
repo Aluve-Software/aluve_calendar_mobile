9b62b743da2a6e2ab8d3212305760aef8032e787
Sibusiso <sbudahnkoane1@gmail.com>
2023-10-11 02:54:30 +0200

    Add app text field file for reusable text field widget

---
+++ lib/Views/Widgets/app_text_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatefulWidget {
+  const AppTextField({super.key});
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
