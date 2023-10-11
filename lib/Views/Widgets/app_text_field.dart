d47ad795144fb45003463e9e504a658e86bddd22
Sibusiso <sbudahnkoane1@gmail.com>
2023-10-11 03:53:55 +0200

    Add code for reusable text field with different parameters

--- lib/Views/Widgets/app_text_field.dart
+++ lib/Views/Widgets/app_text_field.dart
@@ -1,7 +1,27 @@
+import 'package:aluve_calendar_mobile/Views/Widgets/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextField extends StatefulWidget {
-  const AppTextField({super.key});
+  final String hintText;
+  final bool hideText;
+  final String? Function(String?)? validator;
+  final bool hasTrailingIconButton;
+  final void Function()? onTrailingIconPressed;
+  final bool hasTrailingIcon;
+  final IconData? trailingIcon;
+  final Color? trailingIconColor;
+  const AppTextField({
+    super.key,
+    required this.hintText,
+    this.trailingIcon,
+    required this.hasTrailingIconButton,
+    required this.hasTrailingIcon,
+    this.trailingIconColor,
+    this.onTrailingIconPressed,
+    required this.hideText,
+    required this.validator,
+  });
 
   @override
   State<AppTextField> createState() => _AppTextFieldState();
@@ -10,6 +30,36 @@ class AppTextField extends StatefulWidget {
 class _AppTextFieldState extends State<AppTextField> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        obscureText: widget.hideText,
+        validator: widget.validator,
+        style: GoogleFonts.inter(fontSize: 14),
+        decoration: InputDecoration(
+            hintText: widget.hintText,
+            hintStyle: GoogleFonts.inter(fontStyle: FontStyle.italic),
+            suffixIcon: widget.hasTrailingIconButton == false
+                //=====if the textfield does not have a trailing icon button we display only a normal icon====
+                ? widget.hasTrailingIcon == true
+                    //but if the texfield has a trailing normal icon then we display an icon otherwise we place a sixed box
+                    ? Icon(
+                        widget.trailingIcon,
+                        color: widget.trailingIconColor,
+                      )
+                    : const SizedBox()
+                //====if the textfield does have a trailing icon button we display the IconButton=====
+                : IconButton(
+                    iconSize: 25,
+                    onPressed: widget.onTrailingIconPressed,
+                    icon: Icon(
+                      widget.trailingIcon,
+                      color: widget.trailingIconColor,
+                    ),
+                  ),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+      ),
+    );
   }
 }
